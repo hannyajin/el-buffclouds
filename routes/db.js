460924514ae473496d2111355a0c15fc3efc821b
Hannyajin <talmo.christian@gmail.com>
2014-10-19 03:52:04 +0300

    fixed bugs in Schema, added populate references

--- routes/db.js
+++ routes/db.js
@@ -1,24 +1,25 @@
+/** Configure the database and its dependencies
+  */
+
 var express = require('express');
 var router = express.Router();
 
-
-
 /* Connect to Mongo DataBase */
 var mongoose = require('mongoose');
-var mongouri = require('./private/auth.json').mongo.uri;
+var mongouri = require('../private/auth.json').mongo.uri;  // secret from private file
 
 var mongo_opts = { keepAlive: 1 };
 var db = mongoose.createConnection(mongouri, mongo_opts);
 
 db.on('error', console.log.bind(console, 'db connection error:'));
-db.once('open', function mongo_open() {
+db.once('open', function db_open() {
   console.log("Connected to DataBase.");
 });
 
 
 /** Configure Schemas
   */
-var Schema = db.Schema;
+var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 
 var userSchema = new Schema({
@@ -27,8 +28,8 @@ var userSchema = new Schema({
   username: String,
   password: String,
 
-  clouds: [cloudSchema] // Cloud Ids
-  comments: [commentSchema],
+  clouds: [cloudSchema], // Cloud Ids
+  comments: [ { type: ObjectId, ref: 'Comment'} ],
 
   email_verified: { type: Boolean, default: false }, // if email has been verified
 
@@ -37,12 +38,12 @@ var userSchema = new Schema({
 
 var cloudSchema = new Schema({
   _id: { type: ObjectId, auto: true },
-  creator: userSchema,
-  cloud: cloudSchema, // sub-cloud?
+  creator: { type: ObjectId, ref: 'User' },
+  cloud: { type: ObjectId, ref: 'Cloud' }, // sub-cloud?
 
   title: String,
   desc: String,
-  members: [userSchema], // // User Ids
+  members: [{ type: ObjectId, ref: 'User' }], // // User Ids
   tags: [String],
 
   invites: [String], // emails
@@ -52,27 +53,36 @@ var cloudSchema = new Schema({
 
 var linkSchema = new Schema({
   _id: { type: ObjectId, auto: true },
-  creator: userSchema,
-  cloud: cloudSchema,
+  creator: { type: ObjectId, ref: 'User' },
+  cloud: { type: ObjectId, ref: 'Cloud' },
 
-  title: String
+  title: String,
   pictures: [String], // picture urls
   img_index: { type: Number, default: 0 }, // default first linked picture
   tags: [String],
-  comments: [commentSchema],
+  comments: [ { type: ObjectId, ref: 'Comment'} ],
 
   date: { type: Date, default: Date.now }
 });
 
 var commentSchema = new Schema({
   _id: { type: ObjectId, auto: true },
-  creator: userSchema,
-  link: linkSchema,
+  creator: { type: ObjectId, ref: 'User' },
+  link: { type: ObjectId, ref: 'Link'},
 
   text: String,
   
   date: { type: Date, default: Date.now }
 })
+
+/** Initialize Models
+  */
+var models = {
+  User: db.model('User', userSchema),
+  Cloud: db.model('Cloud', cloudSchema),
+  Link: db.model('Link', linkSchema),
+  Comment: db.model('Comment', commentSchema)
+}
 
 
 /** REST API
@@ -87,5 +97,6 @@ router.get('/', function(req, res) {
 /* Expose outside */
 module.exports = {
   type: 'MongoDB',
-  router: router
+  router: router,
+  models: models
 }
